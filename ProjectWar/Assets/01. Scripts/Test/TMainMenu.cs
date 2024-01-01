dd7fd276e4cdd29e2030497812ee4d9a33861e99
SEH00N <06psh0501@gmail.com>
2024-01-01 23:45:33 +0900

    main menu

---
+++ ProjectWar/Assets/01. Scripts/Test/TMainMenu.cs
@@ -0,0 +1,35 @@
+using TMPro;
+using Unity.Netcode;
+using UnityEngine;
+
+public class TMainMenu : MonoBehaviour
+{
+	[SerializeField] TMP_InputField joinCodeField;
+    
+    private void Start()
+    {
+        HostManager.Instance.OnRoomCreatedEvent += HandleRoomCreated;
+    }
+
+    private void OnDestroy()
+    {
+        HostManager.Instance.OnRoomCreatedEvent -= HandleRoomCreated;
+    }
+
+    public async void CreateRoom()
+    {
+        await HostManager.Instance.CreateRoomAsync();
+    }
+
+    public async void JoinRoom()
+    {
+        await ClientManager.Instance.JoinRoomAsync(joinCodeField.text);
+    }
+
+    private void HandleRoomCreated()
+    {
+        SceneLoader.Instance.LoadSceneAsync("GameScene", () => {
+            NetworkManager.Singleton.StartHost();
+        });
+    }
+}
