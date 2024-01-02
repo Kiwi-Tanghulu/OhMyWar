b03ac00a941b785a9fa99ba8c87dfec5eba62e39
SEH00N <06psh0501@gmail.com>
2024-01-02 11:32:14 +0900

    map tile

--- ProjectWar/Assets/01. Scripts/Core/IngameManager.cs
+++ ProjectWar/Assets/01. Scripts/Core/IngameManager.cs
@@ -18,33 +18,4 @@ public class IngameManager : NetworkBehaviour
     [field: SerializeField] public Nexus TopNexus { get; private set; } = null;
     [field: SerializeField] public Nexus MidNexus { get; private set; } = null;
     [field: SerializeField] public Nexus BottomNexus { get; private set; } = null;
-
-    [SerializeField] private Player player1 = null;
-    [SerializeField] private Player player2 = null;
-
-    public override void OnNetworkSpawn()
-    {
-        NetworkManager.Singleton.OnClientConnectedCallback += ReadyGameServerRPC;
-    }
-
-    public void CloseGame(Castle loser)
-    {
-
-    }
-
-    [ServerRpc]
-    private void ReadyGameServerRPC(ulong playerID)
-    {
-        NetworkClient client = NetworkManager.Singleton.ConnectedClients[playerID];
-        Player player = client.PlayerObject.GetComponent<Player>();
-        bool isHost = player.IsHost && player.IsOwner;
-
-        if(isHost)
-            player1 = player;
-        else
-            player2 = player;
-
-        if(player1 != null && player2 != null)
-            Debug.Log("Start Game");
-    }
 }
