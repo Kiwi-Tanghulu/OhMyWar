3641921599f2fabf22886f4fbddf66a1573d20c5
JUNSUNG06 <forest2847@gmail.com>
2024-01-02 18:14:17 +0900

    .

--- ProjectWar/Assets/01. Scripts/Test/JTest.cs
+++ ProjectWar/Assets/01. Scripts/Test/JTest.cs
@@ -5,21 +5,5 @@ using UnityEngine;
 
 public class JTest : NetworkBehaviour
 {
-    public NetworkObject unit;
 
-    // Start is called before the first frame update
-    [ServerRpc]
-    public void SpawnServerRpc()
-    {
-        NetworkObject u = Instantiate(unit);
-        u.Spawn();
-        u.transform.position = Vector3.zero + Vector3.forward;
-        u.GetComponent<UnitMovement>().SetTargetPosition(Vector2.right * 100);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
