04a6a3d0d894345fc5da22f8ef6c07215fd2ba46
kjs613 <ggm.kimjiseong613@gmail.com>
2024-01-04 11:39:34 +0900

    teleport4

--- ProjectWar/Assets/01. Scripts/System/Skill/TeleportSkill.cs
+++ ProjectWar/Assets/01. Scripts/System/Skill/TeleportSkill.cs
@@ -14,7 +14,8 @@ public class TeleportSkill : SkillBase
 {
     [SerializeField] private float maxMidY;
     [SerializeField] private float minMidY;
-    [SerializeField] private TelePortPos[] telePortPos;
+    [SerializeField] private TelePortPos[] blueTelePortPos;
+    [SerializeField] private TelePortPos[] redTelePortPos;
     [SerializeField] private float teleportRange;
     [SerializeField] private float teleportDelay;
     [SerializeField] private LayerMask targetLayer;
@@ -26,15 +27,19 @@ public class TeleportSkill : SkillBase
     private float currentPercent;
     private Vector2 playerTeleportPosition;
     private int currentLineIndex;
+
+    private TelePortPos[] currentTeleportPos;
     protected override bool ActiveSkill()
     {
         Debug.Log("TeleportStart");
+
 
         List<Transform> targetUnits = new List<Transform>();
         units = new List<Transform>();
         unitDistances = new List<Vector2>();
         currentPercent = 0f;
         string target = player.IsBlue ? "BlueUnit" : "RedUnit";
+        currentTeleportPos = player.IsBlue ? blueTelePortPos : redTelePortPos;
         Collider2D[] cols = Physics2D.OverlapCircleAll(player.transform.position, teleportRange);
 
         foreach (Collider2D col in cols)
@@ -59,15 +64,15 @@ public class TeleportSkill : SkillBase
         
         currentLineIndex = player.transform.position.y > maxMidY ? 2 : player.transform.position.y < minMidY ? 0 : 1;
 
-        currentPercent = Mathf.Abs(player.transform.position.x - telePortPos[lineIndex].startPos.x)
-            / Mathf.Abs((telePortPos[lineIndex].endPos.x - telePortPos[lineIndex].startPos.x));
+        currentPercent = Mathf.Abs(player.transform.position.x - currentTeleportPos[lineIndex].startPos.x)
+            / Mathf.Abs((currentTeleportPos[lineIndex].endPos.x - currentTeleportPos[lineIndex].startPos.x));
 
         currentPercent = Mathf.Clamp(currentPercent, 0f, 1f);
         
         Debug.Log(currentPercent);
 
         playerTeleportPosition = 
-           telePortPos[currentLineIndex].startPos + currentPercent * (telePortPos[currentLineIndex].endPos - telePortPos[currentLineIndex].startPos);
+           currentTeleportPos[currentLineIndex].startPos + currentPercent * (currentTeleportPos[currentLineIndex].endPos - currentTeleportPos[currentLineIndex].startPos);
 
         StartCoroutine(TelePortStart());
         
