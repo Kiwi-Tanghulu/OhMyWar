babdf1f292ad7ead1547dcc607683f54b101f9c5
JUNSUNG06 <forest2847@gmail.com>
2024-01-04 18:27:48 +0900

    minimap icon

--- ProjectWar/Assets/01. Scripts/ETC/SightObject.cs
+++ ProjectWar/Assets/01. Scripts/ETC/SightObject.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class SightObject : MonoBehaviour
+public class SightObject : NetworkBehaviour
 {
     [SerializeField] private GameObject minimapPoint;
+    [SerializeField] private Sprite redIcon;
+    [SerializeField] private Sprite blueIcon;
+    [SerializeField] private Color redColor;
+    [SerializeField] private Color blueColor;
+    private SpriteRenderer render;
     private bool isSight;
 
-    private void Start()
+    public override void OnNetworkSpawn()
     {
+        base.OnNetworkSpawn();
+
         isSight = false;
         SetSight(false);
-
+        render = minimapPoint.GetComponent<SpriteRenderer>();
+        Debug.Log(render);
         MinimapManager.Instance.RegistSightObject(this);
+
+        if (IsHost)
+        {
+            render.sprite = blueIcon;
+            render.color = blueColor;
+        }
+        else
+        {
+            render.sprite = redIcon;
+            render.color = redColor;
+        }
     }
 
     public void SetSight(bool value)
